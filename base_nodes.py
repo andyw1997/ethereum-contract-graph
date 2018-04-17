3a19f4b2a074fdf28a5b3c886bbcdca3b0279834
Andy <andyw1997@gmail.com>
2018-04-17 13:55:47 -0700

    fixed base_nodes

--- base_nodes.py
+++ base_nodes.py
@@ -8,12 +8,13 @@ with open('data/contracts.csv', 'r') as file:
 	output = []
 
 	for row in reader:
-		output.append({
-			'id': row[0],
-			'group': group, 
-			'label': row[1], 
-			'level': 1
-			})
-		group += 1
+		if group != 0 and row[3] > 1:
+			output.append({
+				'id': row[0],
+				'group': group, 
+				'label': row[1], 
+				'level': 1
+				})
+			group += 1
 	with open('data/base_nodes.js', 'w+') as file2:
 		json.dump(output,file2)
