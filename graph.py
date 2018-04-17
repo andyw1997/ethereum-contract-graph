02928290a9328e27ff72bd5f0498c00bae5b70d0
Sharabesh Ramesh <sharabesh@berkeley.edu>
2018-04-17 14:05:17 -0700

    more updates

--- graph.py
+++ graph.py
@@ -31,6 +31,26 @@ def make_graph():
 	contracts = {k: v for k, v in contracts.items() if v} # Only graph nodes with edges for now
 	return contracts 
 
+def generate_nodes():
+	with open('data/contracts.csv', 'r') as file:
+		reader = csv.reader(file)
+		group = 0
+
+		output = []
+
+		for row in reader:
+			if group != 0 and int(row[3]) > 1:
+				output.append({
+					'id': row[0],
+					'group': group, 
+					'label': row[1], 
+					'level': 1
+					})
+			group += 1	
+		with open('data/base_nodes.js', 'w+') as file2:
+			json.dump(output,file2)
+
+
 
 def generate_links(graph,filename):
 	output = [] 
